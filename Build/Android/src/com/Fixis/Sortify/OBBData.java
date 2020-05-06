eb274c011b357aa2018aad413c7738785e74d031
Bao <qbao500@gmail.com>
2020-05-06 22:01:28 +0300

    obb

--- Build/Android/src/com/Fixis/Sortify/OBBData.java
+++ Build/Android/src/com/Fixis/Sortify/OBBData.java
@@ -18,8 +18,8 @@ mFileSize = fileSize;
 public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
-"1", // the version of the APK that the file was uploaded against
-0L // the length of the file in bytes
+"9", // the version of the APK that the file was uploaded against
+68557593L // the length of the file in bytes
 )
 };
 };
